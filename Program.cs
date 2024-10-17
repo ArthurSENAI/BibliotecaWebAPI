bbeccac75206ee7a5f8737900e58bc014c5c13f2
ArthurSENAI <arthur.s.araujo7@aluno.senai.br>
2024-10-17 17:31:11 -0300

    aula1710

--- Program.cs
+++ Program.cs
@@ -97,7 +97,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ATMWebAPI v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BibliotecaWebAPI v1"));
 }
 
 app.UseHttpsRedirection();
